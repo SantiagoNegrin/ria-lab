fc5810f0ad2378d65486573dbe48d4f70abd4050
santiago <santiago.negrin@estudiantes.utec.edu.uy>
2023-06-30 17:43:56 -0300

    Signed-off-by: santiago <santiago.negrin@estudiantes.utec.edu.uy>

--- src/app/listar-llamados/listar-llamados.component.ts
+++ src/app/listar-llamados/listar-llamados.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-listar-llamados',
   templateUrl: './listar-llamados.component.html',
   styleUrls: ['./listar-llamados.component.css']
 })
-export class ListarLlamadosComponent {
+export class ListarLlamadosComponent implements OnInit {
+  llamados: any[]= [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getListarLlamados();
+  }
+
+  getListarLlamados() {
+    const url = 'http://localhost:5000/api/Llamados/Paged';
+    const body = {
+      limit: 100,
+      offset: 0,
+      id: 0,
+      filters: {
+        activo: true,
+        nombre: '',
+        identificador: '',
+        personaTribunalId: 0,
+        estadoId: 0
+      },
+      orders: [
+        'string'
+      ]
+    };
+  
+    this.http.post<any>(url, body)
+      .subscribe(response => {
+        console.log(response); // Agrega esta línea para mostrar la respuesta en la consola
+        this.llamados = response.list;
+      });
+  }
+  
 }
